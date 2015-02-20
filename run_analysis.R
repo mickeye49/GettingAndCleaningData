4d0c2e2dc544ef1a301cbe894687927463e423a8
mickeye <mickeye49@gmail.com>
2015-02-20 11:59:14 -0500

    Change the way column names are handled.

--- run_analysis.R
+++ run_analysis.R
@@ -26,52 +26,54 @@ features <- read.table("features.txt", col.names=(c("colno", "colname")),
 #
 # read the test data
 #
-X_test <- read.table("./test/X_test.txt", col.names = features$colname)
-testSubject <- read.table("./test/subject_test.txt", col.names = "subject")
-testActivity <- read.table("./test/y_test.txt", col.names = "activity")
+X_test <- read.table("./test/X_test.txt")
+testSubject <- read.table("./test/subject_test.txt")
+testActivity <- read.table("./test/y_test.txt")
 
 # combine the test data
 allTest <- cbind(testActivity, testSubject, X_test)
 
-# clean up some data no longer needed
-#rm(X_test, testSubject, testActivity, testFactor)
+# remove data no longer needed
+rm(X_test, testSubject, testActivity)
 
 #
 # read the train data
 #
-X_train <- read.table("./train/X_train.txt", col.names = features$colname)
-trainSubject <- read.table("./train/subject_train.txt", col.names = "subject")
-trainActivity <- read.table("./train/y_train.txt", col.names = "activity")
+X_train <- read.table("./train/X_train.txt")
+trainSubject <- read.table("./train/subject_train.txt")
+trainActivity <- read.table("./train/y_train.txt")
 
 # combine the train data
 allTrain <- cbind(trainActivity, trainSubject, X_train)
 
-# clean up some data no longer needed
-#rm(X_train, trainSubject, trainActivity, trainFactor)
+# remove data no longer needed
+rm(X_train, trainSubject, trainActivity)
 
 #
 # combine the test and train data
 #
 allData <- rbind(allTest, allTrain)
 
-# change the activity column to a factor using the activity object
-allData$activity <- factor(allData$activity, levels=actLabels$actno,
-                      labels=actLabels$actname)
+# remove data no longer needed
+rm(allTest, allTrain)
 
 #
 # create a logical vector used to select the columns for the final data set
 #
 selCols <- grepl(".*std\\(\\).*|.*mean\\(\\).*|.*meanFreq\\(\\).*", features$colname)
 
-# select the standard deviation and mean measurements
+# select the activity, subject, and standard deviation and mean measurements
 allStdMean <- allData[, c(TRUE, TRUE, selCols)]
+
+# change the activity column to a factor using the
+# data from the activity_labels.txt file
+allStdMean[,1] <- factor(allStdMean[,1], levels=actLabels$actno,
+                      labels=actLabels$actname)
 
 #
 # get the column names and clean them
 #
 tidyCols <- features$colname[selCols]
-tidyCols <- sub("^([f])(.*)", "F\\2", tidyCols)         # uppercase f at beginning of name
-tidyCols <- sub("^([t])(.*)", "T\\2", tidyCols)         # uppercase t at beginning of name
 tidyCols <- sub("-std\\(\\)", "Std", tidyCols)          # change -std() to Std
 tidyCols <- sub("-mean\\(\\)", "Mean", tidyCols)        # change -mean() to Mean
 tidyCols <- sub("-meanFreq\\(\\)", "MeanFreq", tidyCols) # change meanFreq() to MeanFreq
@@ -92,23 +94,15 @@ write.table(allStdMean, file = "tidyData1.txt", row.name = FALSE)
 # create a dataset with the average of each variable for
 # activity and subject
 #
-
 tidyData2 <- group_by(allStdMean, activity, subject)    # group data by activity and subject
 tidyData2 <- summarise_each(tidyData2, funs(mean))      # calculate average for each group
 
-tidyCols2 <- sub("^", "avg", tidyCols)                  # add avg to column names
-colnames(tidyData2) <- c("activity", "subject", tidyCols2)
+# change the column names
+tidyCols2 <- sub("^([f])(.*)", "avgF\\2", tidyCols)         # uppercase f at beginning of name
+tidyCols2 <- sub("^([t])(.*)", "avgT\\2", tidyCols2)        # uppercase t at beginning of name
+colnames(tidyData2) <- c("activity", "subject", tidyCols2) # set the column names
 
 #
-# write the tidyData2 to a file
+# write the tidyData2 data to a file
 #
 write.table(tidyData2, file = "tidyData2.txt", row.name = FALSE)
-
-
-mydata <- read.table("tidyData2.txt", header = TRUE)
-
-#
-# clean up remaining data
-#
-#rm(list=ls())
-
